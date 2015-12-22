4ff35b9a68aef9cc4a65331e84bc71502463ddab
Grant Kinney <grant@exygy.com>
2015-12-22 11:15:35 -0500

    Remove CSRF protection for json calls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+
+  # Remove CSRF protection for json requests
+  # http://edgeapi.rubyonrails.org/classes/ActionController/RequestForgeryProtection.html
+  protect_from_forgery unless: -> { request.format.json? }
 end
