d66b00ef88388c93c1e9a922e0697b93e88e750e
Pierre <pierre@exygy.com>
2016-02-10 14:03:06 -0800

    turn protecting from forgery back on

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   rescue_from ActionController::RoutingError, with: :render_not_found
