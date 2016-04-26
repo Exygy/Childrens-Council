260d92b1adae409cdff29472b3dae099c016cc21
Pierre <pierre@exygy.com>
2016-04-26 14:13:01 -0700

    improving referrals

--- app/models/referral_log.rb
+++ app/models/referral_log.rb
@@ -2,11 +2,15 @@
 #
 # Table name: referral_logs
 #
-#  id         :integer          not null, primary key
-#  params     :json
-#  parent_id  :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id                :integer          not null, primary key
+#  params            :json
+#  parent_id         :integer          not null
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  child_age_months  :integer
+#  schedule_week_ids :integer          default([]), is an Array
+#  schedule_year_id  :integer
+#  care_reason_ids   :integer          default([]), is an Array
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411220403) do
+ActiveRecord::Schema.define(version: 20160426184233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -307,9 +307,13 @@ ActiveRecord::Schema.define(version: 20160411220403) do
 
   create_table "referral_logs", force: :cascade do |t|
     t.json     "params"
-    t.integer  "parent_id",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "parent_id",                      null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "child_age_months"
+    t.integer  "schedule_week_ids", default: [],              array: true
+    t.integer  "schedule_year_id"
+    t.integer  "care_reason_ids",   default: [],              array: true
   end
 
   add_index "referral_logs", ["parent_id"], name: "index_referral_logs_on_parent_id", using: :btree
@@ -373,6 +377,26 @@ ActiveRecord::Schema.define(version: 20160411220403) do
     t.text     "description"
     t.boolean  "display",     default: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
     t.string   "item_type",  null: false
