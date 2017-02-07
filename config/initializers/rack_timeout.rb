b7d47df4edd448dfe3770da7ae679b1ba089a269
Pierre <pierre@exygy.com>
2017-02-07 14:54:10 -0800

    rack timeout only for production

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,3 +1,5 @@
-Rack::Timeout.service_timeout = 5
-Rack::Timeout.wait_timeout = 5
-Rack::Timeout.wait_overtime = 5
+if Rails.env.production?
+  Rack::Timeout.service_timeout = 5
+  Rack::Timeout.wait_timeout = 5
+  Rack::Timeout.wait_overtime = 5
+end
