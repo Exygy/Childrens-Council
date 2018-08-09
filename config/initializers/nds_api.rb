cd0b90b51d85aed00391f24936ef1f42d0ef39c9
Pierre Hunault <pierre@exygy.com>
2018-08-08 17:22:51 -0700

    collectinf referrals

--- config/initializers/nds_api.rb
+++ config/initializers/nds_api.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+NDS_AGENCY_KEY = ENV['NDS_AGENCY_KEY']
+
 NDS = NdsApi::Client.new(
-  agency_key: 13_011,
+  agency_key: NDS_AGENCY_KEY || 13_011,
   user: 'exygy',
   password: 'testnaccrra',
+  # password: 'SnowflakeFrog?',
   dev: true
 )
