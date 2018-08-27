51127019a7185a48d2f6d7be3e37b11431077272
Marcin Jedras <mjjedras@gmail.com>
2018-08-27 14:12:31 +0200

    Make migration reversible

--- db/migrate/20180822164003_change_parents_constraints.rb
+++ db/migrate/20180822164003_change_parents_constraints.rb
@@ -1,6 +1,11 @@
 class ChangeParentsConstraints < ActiveRecord::Migration
-  def change
+  def up
     change_column :parents, :first_name, :text, null: true
     change_column :parents, :last_name, :text, null: true
+  end
+
+  def down
+    change_column :parents, :first_name, :text, null: false
+    change_column :parents, :last_name, :text, null: false
   end
 end
