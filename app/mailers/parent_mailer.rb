8f672b50716fda1fa2cfdea36b7d47e8d5e70aae
Catherine Callaghan <catherine.callaghan@exygy.com>
2018-09-05 10:30:13 -0400

    Fix typo in Children's Council name in welcome email

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -1,7 +1,6 @@
 class ParentMailer < ApplicationMailer
-
   def welcome_email(parent_id)
     @parent = Parent.find(parent_id)
-    mail(to: @parent.email, subject: 'Welcome to Children Council')
+    mail(to: @parent.email, subject: "Welcome to Children's Council")
   end
 end
