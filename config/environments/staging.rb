58867515dad45c358b39883179d659aafa3dde50
Marcin Jedras <mjjedras@gmail.com>
2018-09-13 15:18:10 +0200

    Add mailer host url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -111,4 +111,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = {host: 'ccsf.wpengine.com'}
 end
