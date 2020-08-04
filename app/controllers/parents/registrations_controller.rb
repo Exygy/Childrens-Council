50e53ef096b8f62da2c018ea8937429e4258ffb9
Pierre Hunault <pierre@exygy.com>
2020-08-04 11:55:41 -0700

    drop care reason

--- app/controllers/parents/registrations_controller.rb
+++ app/controllers/parents/registrations_controller.rb
@@ -66,7 +66,7 @@ class Parents::RegistrationsController < DeviseTokenAuth::RegistrationsControlle
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:api_key, :home_zip_code, :full_name, :care_reason, :found_option])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:api_key, :home_zip_code, :full_name, :found_option])
   end
 
   def configure_account_update_params
