57330599cdbca9618cfd4fdbc0d2cb4155d148f8
Pierre Hunault <pierre@exygy.com>
2020-08-19 11:55:12 -0700

    adding back the pre school filter

--- config/initializers/nds_api.rb
+++ config/initializers/nds_api.rb
@@ -1,7 +1,7 @@
 NDS = NdsApi::Client.new(
   agency_key: ENV['NDS_API_AGENCY_KEY'] || 13_011,
-  user: ENV['NDS_API_USERNAME'] || 'exygy',
-  password: ENV['NDS_API_PASSWORD'] || 'testnaccrra',
+  user: ENV['NDS_API_USERNAME'] || 'exygy_api@13011',
+  password: ENV['NDS_API_PASSWORD'] || 'NdibtG6H2mc2XGW7YP',
   dev: ENV['NDS_API_DEV'] ? ENV['NDS_API_DEV'] == "true" : true,
   debug: ENV['NDS_API_DEBUG'] ? ENV['NDS_API_DEBUG'] == "true" : false
 )
